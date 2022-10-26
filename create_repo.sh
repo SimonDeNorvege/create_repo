edbb47fbde054a400fa9c7c4be1514ec3dea555e
Pierre <pierre.roumane@epitech.eu>
2022-10-26 05:19:49 -0400

    <feat> (1.2) added function for the padding of the file

--- create_repo.sh
+++ create_repo.sh
@@ -1,37 +1,57 @@
 #/bin/bash
 
-function packing { #fonction de remplissage de fichier
+function packing {  #fonction de remplissage de fichier
 
 IFS='.' #Internal Field Seperator pour couper le nom du fichier
 
 if [ "$1" = "Makefile" ]
 then
-    return  #Makefile
+    packing_makefile  #Makefile
 else
     set $1 #reduit le nom du fichier pour avoir juste apres le point
     if [ "$2" = "c" ] # $2 représente ce qui est écrit après le point
     then
-        return #C
+        packing_C #C
     elif [ "$2" = "java" ]
     then
-        return #java
+        packing_java #java
     elif [ "$2" = "sh" ]
     then
-        return #bash
+        packing_sh #bash
     fi 
 fi
-echo "Nombres de paramètres : $#"
-echo "premier parametre : $1"
-echo "deuxieme parametre : $2"
 }
+
+<< 'Comment'
+    the packing function starts here, these are the functions 
+    that fill the files created during the repository creation
+Comment
+
+function packing_makefile { #fonction de remplissage de Makefile 
+return
+}
+
+function packing_C { #fonction de remplissage des fichiers C
+return
+}
+
+function packing_java { #fonction de remplissage du fichier Java
+return
+}
+
+function packing_sh { #fonction de remplissage du fichier Bash
+return
+}
+
+# the file packing ends here
 
 if [ "$1" = "-h" ]
 then
     echo ""
-    echo "Prepares your GitHub repository Java/C/Bash projects :" 
+    echo "Prepares your GitHub repository : Java/C/Bash projects :" 
     echo "\$1 is the project name"
     echo ""
-    exit 0
+    return 0
 fi
 
 name=false
@@ -125,7 +145,7 @@ then
         packing src/$1.sh
 
     else
-        echo "Je ne connais pas encore ce language!"
+        echo "Je ne connais pas encore ce language!" #Erreur 
 
     fi
 else 
@@ -135,3 +155,4 @@ fi
 
 
 echo "done!"
+return 0
