c2b9d439a830ace599624c3b7f6173372489294f
Pierre <pierre.roumane@epitech.eu>
2022-10-26 03:54:34 -0400

    <feat> updated create_repo.sh

--- create_repo.sh
+++ create_repo.sh
@@ -1,5 +1,30 @@
 #/bin/bash
 
+function packing {
+
+IFS='.' #Internal Field Seperator S pour couper le nom du fichier
+
+if [ "$1" = "Makefile" ]
+then
+    return  #Makefile
+else
+    set $1 #reduit le nom du fichier pour avoir juste apres le point
+    if [ "$2" = "c" ] # $2 représente ce qui est écrit après le point
+    then
+        return #C
+    elif [ "$2" = "java" ]
+    then
+        return #java
+    elif [ "$2" = "sh" ]
+    then
+        return #bash
+    fi 
+fi
+echo "Nombres de paramètres : $#"
+echo "premier parametre : $1"
+echo "deuxieme parametre : $2"
+}
+
 if [ "$1" = "-h" ]
 then
     echo ""
@@ -68,7 +93,7 @@ fi
 touch Makefile
 
 #Packing of Makefile
-#Under construction
+packing Makefile
 
 # Check si le nom du projet à été donné
 if [ name=true ]
@@ -76,27 +101,28 @@ then
     echo "The projet is in which language?"
     read
 
+    echo "$REPLY"
     if [ $REPLY = "c" -o $REPLY = "C" ]
     then #C project
         touch include/$1.h #!Creation of .h
         touch src/$1.c #!Creation of .c
 
         #Packing of src
-        #Under Construction
+        packing src/$1.c
 
     elif [ $REPLY = "java" -o $REPLY = "Java" ]
     then #Java project
         touch src/$1.java #!Creation of .java
 
         #Packing of src
-        #Under Construction
+        packing src/$1.java
 
-    elif [ $REPLY = "bash" -o $REPLY = "Bash"]
+    elif [ $REPLY = "bash" -o $REPLY = "Bash" -o $REPLY = "sh"]
     then #Bash project
         touch src/$1.sh #Creation of .sh
 
         #Packing of src
-        #Under Construction
+        packing src/$1.sh
 
     else
         echo "Je ne connais pas encore ce language!"
@@ -105,7 +131,6 @@ then
 else 
     echo "Les fichiers n'ont pas été crées"
 fi
-
 
 
 
